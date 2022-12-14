7cdcc3442a6f917eb2e570b528d1f6ea558b5847
wnsxor1993 <>
2022-12-14 14:40:15 +0900

    feat: Kakao 로그인 구현

--- SignIn_Up/SignIn_Up/ManageSection/SignInManager.swift
+++ SignIn_Up/SignIn_Up/ManageSection/SignInManager.swift
@@ -12,11 +12,12 @@ open class SignInManager {
     
     let shared = SignInManager()
     let normalWorker = NormalSignInWorker()
+    let kakaoWorker = KakaoSignInWorker()
     
     private init() { }
     
     func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
-//        self.kakaoManager.scene(scene, openURLContexts: URLContexts)
+        self.kakaoWorker.scene(scene, openURLContexts: URLContexts)
     }
 }
 
@@ -24,5 +25,13 @@ extension SignInManager {
     
     func signInNormal(with endPoint: EndPoint) -> Single<NetworkReturnModel> {
         return self.normalWorker.signIn(with: endPoint)
+    }
+    
+    func fetchKakaoToken() -> Single<String> {
+        return self.kakaoWorker.fetchAccessToken()
+    }
+    
+    func signInKakao(with endPoint: EndPoint) -> Single<NetworkReturnModel> {
+        return self.kakaoWorker.signIn(with: endPoint)
     }
 }
--- SignIn_Up/SignIn_Up/ManageSection/SignInSocial/KakaoSignInWorker.swift
+++ SignIn_Up/SignIn_Up/ManageSection/SignInSocial/KakaoSignInWorker.swift
@@ -6,4 +6,70 @@
 //
 
 import RxSwift
+import KakaoSDKAuth
+import KakaoSDKUser
 
+final class KakaoSignInWorker {
+    
+    func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+        guard let url = URLContexts.first?.url else { return }
+        
+        if AuthApi.isKakaoTalkLoginUrl(url) {
+            _ = AuthController.handleOpenUrl(url: url)
+        }
+    }
+    
+    func fetchAccessToken() -> Single<String> {
+        
+        return Single.just(UserApi.isKakaoTalkLoginAvailable())
+            .flatMap { [weak self] isAppLoginAvailable in
+                guard let self else { return .never() }
+                
+                return isAppLoginAvailable ? self.signInWithKakaoApp() : self.signInWithKakaoAccount()
+            }
+    }
+    
+    func signIn(with endPoint: EndPoint) -> Single<NetworkReturnModel> {
+        
+        return NetworkProvider.request(with: endPoint)
+    }
+}
+
+private extension KakaoSignInWorker {
+    
+    func signInWithKakaoApp() -> Single<String> {
+        
+        return .create { observer in
+            UserApi.shared.loginWithKakaoTalk { (oauthToken, error) in
+                guard let oauthToken = oauthToken else {
+                    NSLog("KakaoTalk Login error: \(String(describing: error))")
+                    observer(.failure(error ?? RxError.noElements))
+                    
+                    return
+                }
+                
+                observer(.success(oauthToken.accessToken))
+            }
+            
+            return Disposables.create()
+        }
+    }
+    
+    func signInWithKakaoAccount() -> Single<String> {
+        
+        return .create { observer in
+            UserApi.shared.loginWithKakaoAccount { (oauthToken, error) in
+                guard let oauthToken = oauthToken else {
+                    NSLog("KakaoTalk Login error: \(String(describing: error))")
+                    observer(.failure(error ?? RxError.noElements))
+                    
+                    return
+                }
+                
+                observer(.success(oauthToken.accessToken))
+            }
+            
+            return Disposables.create()
+        }
+    }
+}
